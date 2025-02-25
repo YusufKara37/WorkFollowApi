816c44f03f8e3cb0a761cef2feb3f41658faf5fa
Yusuf ismail Kara <yusufismailkara074@gmail.com>
2025-02-25 21:59:24 +0300

    work stage default deger atamasi

--- VM/CreateWorkVM.cs
+++ VM/CreateWorkVM.cs
@@ -4,6 +4,7 @@ public class CreateWorkVM
 {
     public string WorkName { get; set; } = null!;
     public string? WorkComment { get; set; }
+    public int? WorkStageId { get; set; } = 3;
     public DateTime WorkStartDate { get; set; } = DateTime.Now;
     public DateTime? WorkAndDate { get; set; } =null;
 
